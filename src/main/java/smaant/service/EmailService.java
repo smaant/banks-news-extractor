40cf1a93dbe49c638e2aabb7c82dcf2171d00154
Anton Panferov <smaant@gmail.com>
2014-12-27 12:03:06 +0300

    fixed bug with reusage of email client

--- src/main/java/smaant/service/EmailService.java
+++ src/main/java/smaant/service/EmailService.java
@@ -5,12 +5,25 @@ import org.apache.commons.mail.HtmlEmail;
 
 public class EmailService {
 
-  private final HtmlEmail email;
+  private final String host;
+  private final String port;
+  private final String username;
+  private final String password;
+  private final String from;
 
   public EmailService(String host, String port, String user, String password, String from) {
-    email = new HtmlEmail();
+    this.host = host;
+    this.port = port;
+    this.username = user;
+    this.password = password;
+    this.from = from;
+  }
+
+  private HtmlEmail getNewEmailClient() {
+    final HtmlEmail email = new HtmlEmail();
+
     email.setHostName(host);
-    email.setAuthentication(user, password);
+    email.setAuthentication(username, password);
     email.setSslSmtpPort(port);
     email.setSSLOnConnect(true);
 
@@ -19,9 +32,12 @@ public class EmailService {
     } catch (EmailException e) {
       throw new RuntimeException(e);
     }
+
+    return email;
   }
 
   public void sendEmail(String to, String subject, String body) {
+    final HtmlEmail email = getNewEmailClient();
     try {
       email.addTo(to);
       email.setSubject(subject);
